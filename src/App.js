5a29809cddd0d512dae36c502419024b02857cee
RaikhanImanbay <imanbayraikhan@gmail.com>
2022-06-10 10:45:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import WeatherComponent from './CurrentWeather';
+import VComponent from './VComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <WeatherComponent />
+      <VComponent fName="Raikhan" lName="Imanbay"/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
