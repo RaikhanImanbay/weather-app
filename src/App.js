e8702a23684ec6619db23728ad95982ef62a824a
RaikhanImanbay <imanbayraikhan@gmail.com>
2022-06-10 18:19:08 +0300

    commit lesson 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import NavComponent from './Header/NavComponent';
 import {Container, Row, Col} from 'react-bootstrap';
-
+import HeaderComponent from './Header/HeaderComponents';
 function App() {
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const city = event.target.city.value;
+    const unit = event.target.unit.value;
+    console.log(city);
+    console.log(unit);
+}
+
   return (
     <Container>
   <Row>
-    <Col><NavComponent /></Col>
+    <Col>
+    <HeaderComponent handleForm = {handleSubmit}/></Col>
   </Row>
 </Container>
   );
