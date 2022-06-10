0d0c15fde0ed2f1f76ba9af3e1fbed87bea8436f
RaikhanImanbay <imanbayraikhan@gmail.com>
2022-06-10 12:24:07 +0300

    second commit lesson 2

--- src/FComponent.js
+++ src/FComponent.js
@@ -1,17 +1,16 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function FComponent(props) {
     const [age, setAge] = useState(0);
-   
+    
     return (
         <div>
-            <button onClick={() =>setAge(age+1)}>Add age
-            </button>
+            <button onClick={() => setAge(age + 1)}>Add age</button>
             <h1>Hello from  {props.fName} {props.lName} age: {age}</h1>
         </div>
     )
 }
 FComponent.defaultProps = {
     fName: "Raikhan",
-     lName: "Imanbay",
+    lName: "Imanbay",
 }
--- src/VComponent.js
+++ src/VComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Menu = () => {
+const VComponent = (props) => {
     return(
         <div>
         <h1>Hello from Arrow Component </h1>
@@ -8,4 +8,4 @@ const Menu = () => {
     )
 }
 
-export default Menu;
+export default VComponent;
