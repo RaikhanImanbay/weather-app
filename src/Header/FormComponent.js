59ef9268871738efc55032958602581ed30ae3df
RaikhanImanbay <imanbayraikhan@gmail.com>
2022-06-11 19:50:41 +0300

    homework lesson 3

--- src/Header/FormComponent.js
+++ src/Header/FormComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import cities from "./cities.json";
+import dataTypes from "./type.json";
 
 export default function FormComponent(props) {
 
@@ -8,6 +9,22 @@ export default function FormComponent(props) {
     return (
         <Container>
             <Form onSubmit={props.handleForm}>
+                {dataTypes.map(dtype =>
+                    <Form.Check
+                        key={dtype.value}
+                        type="checkbox"
+                        name="dataType"
+                        label={dtype.label}
+                        value={dtype.value}
+                    />
+                )}
+                <Form.Select name="city" aria-label="Default select example">
+                    <option>Choose language</option>
+                    {["en", "kz", "ru"].map(language =>
+                        <option key={language}>{language}</option>
+                    )}
+                </Form.Select>
+
                 <Form.Group className="mb-3" controlId="formGroupEmail">
                     <Form.Label>Choose city</Form.Label>
                     <Form.Select name="city" aria-label="Default select example">
