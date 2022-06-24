4b9336fa1611f87c940d9240bd6538070e829234
RaikhanImanbay <raikhan.imanbay@gmail.com>
2022-06-24 20:42:33 +0300

    leeson 6

--- src/Header/FormComponent.js
+++ src/Header/FormComponent.js
@@ -2,93 +2,110 @@ import React, { useRef, useEffect } from "react";
 import { Container, Form, Row, Col } from "react-bootstrap";
 import cities from "../Header/cities.json";
 import dataTypes from "./type.json";
-import 'dotenv/config';
 
 export default function FormComponent(props) {
 
-	const formElement = useRef(null);
+  const formElement = useRef(null);
 
-  useEffect(()=> {
-    if(props.form === null) {
-    	props.setForm({
-			city: props.selectedCity,
-			unit: props.unit,
-			language: props.language
-		});
-  }});
+  useEffect(() => {
+    if (props.form === null) {
+      props.setForm({
+        city: props.selectedCity,
+        unit: props.unit,
+        language: props.language
+      });
+    }
+  });
 
-	function handleInput() {
-		console.log(formElement);
+  function handleInput() {
+    formElement.current.dispatchEvent(
+      new Event('submit', { cancelable: true, bubbles: true })
+    )
+  }
 
-		formElement.current.dispatchEvent(
-			new Event('submit', { cancelable: true, bubbles: true })
-		)
-	}
+  return (
+    <Container>
+      <Form ref={formElement} onInput={handleInput} onSubmit={props.handleForm}>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="city">
+              <Form.Label>Choose city</Form.Label>
+              <Form.Select defaultValue={(props.cookie || {}).city || props.selectedCity} name="city" aria-label="Default select example">
+                <option>Open this select menu</option>
+                {cities.map((city, i) =>
+                  <option value={i} key={city.name}>{city.name}</option>
+                )}
+              </Form.Select>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="language">
+              <Form.Label>Choose language</Form.Label>
+              <Form.Select defaultValue={(props.cookie || {}).language || props.language} name="language" aria-label="Default select example">
+                <option>Open this select menu</option>
+                {["en", "kz", "ru"].map(language =>
+                  <option key={language}>{language}</option>
+                )}
+              </Form.Select>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="data-type">
+              <Form.Label>Choose Data type</Form.Label>
+              {dataTypes.map(dtype => {
+                let isDataChecked = props.dataType.value === dtype.value;
+                if((props.cookie || {}).excludeDataType) {
+                  let isExcluded = props.cookie.excludeDataType.find(type
 
-	return (
-		<Container>
-			<Form ref={formElement} onInput={handleInput} onSubmit={props.handleForm}>
-				<Row>
-					<Col>
-						<Form.Group className="mb-3" controlId="city">
-							<Form.Label>Choose city</Form.Label>
-							<Form.Select defaultValue={props.selectedCity} name="city" aria-label="Default select example">
-								<option>Open this select menu</option>
-								{cities.map((city, i) =>
-									<option value={i} key={city.name}>{city.name}</option>
-								)}
-							</Form.Select>
-						</Form.Group>
-					</Col>
-					<Col>  <Form.Group className="mb-3" controlId="language">
-						<Form.Label>Choose language</Form.Label>
-						<Form.Select defaultValue={props.language} name="language" aria-label="Default select example">
-							<option>Open this select menu</option>
-							{["en", "kz", "ru"].map(language =>
-								<option key={language}>{language}</option>
-							)}
-						</Form.Select>
-					</Form.Group>
-					</Col>
-				</Row>
-				<Row>
-					<Col>             <Form.Group className="mb-3" controlId="data-type">
-						<Form.Label>Choose Data type</Form.Label>
-						{dataTypes.map(dtype =>
-							<Form.Check
-								key={dtype.value}
-								id={dtype.value}
-								type="checkbox"
-								name="dataType"
-								defaultChecked={props.dataType.value === dtype.value}
-								label={dtype.label}
-								value={dtype.value}
-							/>
-						)}
-					</Form.Group></Col>
-					<Col>        <Form.Group controlId="formGroupRadio">
-						<Form.Label>Choose unit</Form.Label>
-						{['standard', 'metric', 'imperial'].map(unit =>
-							<Form.Check
-								key={unit}
-								id={unit}
-								name="unit"
-								type="radio"
-								label={unit}
-								value={unit}
-								defaultChecked={props.unit === unit}
-							/>
-						)}
-					</Form.Group></Col>
-				</Row>
-			</Form>
-		</Container>
-	)
+
+                  )
+
+                }
+
+                return(
+                  <Form.Check
+                  key={dtype.value}
+                  id={dtype.value}
+                  type="checkbox"
+                  name="dataType"
+                  defaultChecked={isDataChecked}
+                  label={dtype.label}
+                  value={dtype.value}
+                />
+                )}
+              )}
+            </Form.Group></Col>
+          <Col>
+            <Form.Group controlId="formGroupRadio">
+              <Form.Label>Choose unit</Form.Label>
+              {['standard', 'metric', 'imperial'].map(unit => {
+                let isUnitChecked = props.unit === unit;
+                if((props.cookie || {}).unit) {
+                  isUnitChecked = props.cookie.unit === unit;
+                }
+                return (<Form.Check
+                  key={unit}
+                  id={unit}
+                  name="unit"
+                  type="radio"
+                  label={unit}
+                  value={unit}
+                  defaultChecked={isUnitChecked}
+                />)
+              }
+              )}
+            </Form.Group></Col>
+        </Row>
+      </Form>
+    </Container>
+  )
 }
 
 FormComponent.defaultProps = {
-	selectedCity: 0,
-	language: "en",
-	dataType: dataTypes.find(type => type.label === "Daily"),
-	unit: "metric"
+  selectedCity: 0,
+  language: "en",
+  dataType: dataTypes.find(type => type.label === "Daily"),
+  unit: "metric"
 }
